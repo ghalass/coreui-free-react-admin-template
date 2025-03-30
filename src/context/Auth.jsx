6d21dcf9ce086d8b759e43c284949f3a2207bf11
MED SALEM GHALASS <msghalas@gmail.com>
2025-03-30 18:39:17 +0000

    remove all unused files

--- src/context/Auth.jsx
+++ src/context/Auth.jsx
@@ -1,19 +1,19 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState } from 'react'
 
-const AuthContext = createContext(null);
+const AuthContext = createContext(null)
 
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-  const [token, setToken] = useState(null);
+  const [user, setUser] = useState(null)
+  const [token, setToken] = useState(null)
 
-  const login = (user) => setUser(user);
-  const logout = () => setUser(null);
+  const login = (user) => setUser(user)
+  const logout = () => setUser(null)
 
-  const value = { user, token, setToken, login, logout };
+  const value = { user, token, setToken, login, logout }
 
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
-};
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+}
 
-const useAuth = () => useContext(AuthContext);
+const useAuth = () => useContext(AuthContext)
 
-export { AuthContext, AuthProvider, useAuth };
+export { AuthContext, AuthProvider, useAuth }
--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,27 @@
 import React from 'react'
-const Home = React.lazy(() => import('./views/home/Home'))
-const Sites = React.lazy(() => import('./views/configs/Sites'))
-const Typeparcs = React.lazy(() => import('./views/configs/Typeparcs'))
-const Parcs = React.lazy(() => import('./views/configs/Parcs'))
-const Engins = React.lazy(() => import('./views/configs/Engins'))
-const Typelubrifiants = React.lazy(() => import('./views/configs/Typelubrifiants'))
-const Lubrifiants = React.lazy(() => import('./views/configs/Lubrifiants'))
-// 
-const SaisieRje = React.lazy(() => import('./views/saisies/SaisieRje'))
-const SaisieRjeDonnees = React.lazy(() => import('./views/saisies/SaisieRjeDonnees'))
-const EtatMensuel = React.lazy(() => import('./views/rapports_performances/EtatMensuel'))
-const HeuresChassis = React.lazy(() => import('./views/rapports_performances/HeuresChassis'))
-const ParetosInDispo = React.lazy(() => import('./views/rapports_performances/ParetosInDispo'))
-const RapportIndispo = React.lazy(() => import('./views/rapports_performances/RapportIndispo'))
-const RapportRje = React.lazy(() => import('./views/rapports_performances/RapportRje'))
-const RapportSpecLub = React.lazy(() => import('./views/rapports_performances/RapportSpecLub'))
-const UnitePhysique = React.lazy(() => import('./views/rapports_performances/UnitePhysique'))
-const Profile = React.lazy(() => import('./views/pages/user/Profile'))
+
+const Home = React.lazy(() => import('./pages/home/Home'))
+const Sites = React.lazy(() => import('./pages/configs/Sites'))
+const Typeparcs = React.lazy(() => import('./pages/configs/Typeparcs'))
+const Parcs = React.lazy(() => import('./pages/configs/Parcs'))
+const Engins = React.lazy(() => import('./pages/configs/Engins'))
+const Typelubrifiants = React.lazy(() => import('./pages/configs/Typelubrifiants'))
+const Lubrifiants = React.lazy(() => import('./pages/configs/Lubrifiants'))
+const SaisieRje = React.lazy(() => import('./pages/saisies/SaisieRje'))
+const SaisieRjeDonnees = React.lazy(() => import('./pages/saisies/SaisieRjeDonnees'))
+const EtatMensuel = React.lazy(() => import('./pages/rapports_performances/EtatMensuel'))
+const HeuresChassis = React.lazy(() => import('./pages/rapports_performances/HeuresChassis'))
+const ParetosInDispo = React.lazy(() => import('./pages/rapports_performances/ParetosInDispo'))
+const RapportIndispo = React.lazy(() => import('./pages/rapports_performances/RapportIndispo'))
+const RapportRje = React.lazy(() => import('./pages/rapports_performances/RapportRje'))
+const RapportSpecLub = React.lazy(() => import('./pages/rapports_performances/RapportSpecLub'))
+const UnitePhysique = React.lazy(() => import('./pages/rapports_performances/UnitePhysique'))
+const Profile = React.lazy(() => import('./pages/user/Profile'))
+const Page404 = React.lazy(() => import('./pages/page404/Page404'))
 
 const routes = [
   { path: '/', name: 'Home', element: Home },
+  { path: '/*', name: 'Page404', element: Page404 },
 
   { path: '/user/profile', name: 'Profile', element: Profile },
 
